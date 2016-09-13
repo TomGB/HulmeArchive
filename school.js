7e53c648f26da94f67e03d0a25e249481ea3793c
Tom Banister <tgbmac@Toms-MacBook-Air.local>
2016-09-13 23:24:00 +0100

    put all current files on git

---
+++ school.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $(".mobile_menu").click(function () {
+    if(!$("nav.mobile_nav").hasClass("active")){
+      $("nav.mobile_nav").addClass("active");
+    }else{
+      $("nav.mobile_nav").removeClass("active");
+    }
+  });
+
+  $("nav.desktop_nav .drop_down").on('mouseenter', function() {
+    $(this).children(".sub_nav").addClass("active");
+  }).on('mouseleave', function(){
+      $(".sub_nav").removeClass("active");
+  });
+
+  $("nav.mobile_nav .drop_down").on('click', function() {
+    $(".sub_nav").removeClass("active");
+    $(this).children(".sub_nav").slideToggle();
+  })
+});
